403c33f1ee5bc14bbc91d71e860c90941e37f636
2218675712 <2218675712@qq.com>
2020-09-16 19:38:22 +0800

    2020年09月16日 19:37:50 uni-ui引用

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,12 +1,17 @@
 <template>
-<view>首页
-  <text class="iconfont iconimg"></text>
-</view>
+  <view>首页
+    <text class="iconfont iconimg"></text>
+    <uni-badge text="1"></uni-badge>
+
+  </view>
 </template>
 
 <script>
+import {uniBadge} from '@dcloudio/uni-ui'
+
 export default {
-name: "index"
+  name: "index",
+  components: {uniBadge}
 }
 </script>
 
