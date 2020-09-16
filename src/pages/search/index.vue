57caa66c4c9546f168d74e77cb95b6ac632349bc
2218675712 <2218675712@qq.com>
2020-09-16 19:20:06 +0800

    2020年09月16日 19:19:51 项目环境搭建

---
+++ src/pages/search/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>搜索</view>
+
+</template>
+
+<script>
+export default {
+name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/video/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>视频</view>
+
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
