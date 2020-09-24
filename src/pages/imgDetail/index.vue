d467459d2fe82dcfc2adfc6b9544414efb4e8dfc
2218675712 <2218675712@qq.com>
2020-09-24 23:51:21 +0800

    2020年09月24日 23:51:12 专辑模块

--- src/pages/imgDetail/index.vue
+++ src/pages/imgDetail/index.vue
@@ -7,7 +7,7 @@
       </view>
       <view class="user_desc">
         <view class="user_name">{{imgDetail.user.name}}</view>
-        <view class="user_time">{{imgDetail.atime}}</view>
+        <view class="user_time">{{imgDetail.cnTime}}</view>
       </view>
     </view>
     <!--    高清大图-->
@@ -23,23 +23,66 @@
         <text class="iconfont iconshoucang">收藏</text>
       </view>
     </view>
+    <!--    专辑-->
+    <view class="album_wrap">
+      <view class="album_title"></view>
+      <view class="album_list">
+        <view class="album_item" v-for="item in album" :key="item.id">
+          <view class="album_cover">
+            <image :src="item.cover" mode="aspectFill"></image>
+          </view>
+          <view class="album_info">
+            <view class="album_info_text">专辑</view>
+            <view class="album_info_name">{{item.name}}</view>
+            <text class="iconfont iconiconfontjiantou4"></text>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+import moment from "moment";
+
+moment.locale("zh-cn");
 export default {
   name: 'index',
   data() {
     return {
       // 图片信息对象
-      imgDetail: {}
+      imgDetail: {},
+      // 专辑数据数组
+      album: [],
+      // 最热评论
+      hot: [],
+      // 最新评论
+      comment: []
     }
   },
   onLoad: function () {
     const {imgList, imgIndex} = getApp().globalData
     this.imgDetail = imgList[imgIndex]
     this.imgDetail.newThumb = this.imgDetail.thumb + this.imgDetail.rule.replace('$<Height>', 375)
-
+    // XX年前的数据
+    this.imgDetail.cnTime = moment(this.imgDetail.atime * 1000).fromNow()
+    // 获取图片评论
+    this.getComments(this.imgDetail.id)
+  },
+  methods: {
+    /**
+     * 获取图片评论
+     * @param id
+     */
+    getComments(id) {
+      this.request({
+        url: `http://157.122.54.189:9088/image/v2/wallpaper/wallpaper/${id}/comment`,
+      }).then(result => {
+        this.album = result.res.album
+        this.hot = result.res.hot
+        this.comment = result.res.comment
+      })
+    }
   }
 }
 </script>
@@ -76,11 +119,13 @@ export default {
   display: flex;
   height: 80rpx;
   border-bottom: 5rpx solid #eeeeee;
+
   .rank {
     display: flex;
     justify-content: center;
     align-items: center;
     flex: 1;
+
     .iconfont {
 
     }
@@ -91,8 +136,64 @@ export default {
     justify-content: center;
     align-items: center;
     flex: 1;
+
     .iconfont {
 
+    }
+  }
+}
+
+.album_wrap {
+  padding: 20rpx;
+
+  .album_title {
+    padding: 10rpx 0;
+  }
+
+  .album_list {
+
+    .album_item {
+      display: flex;
+      padding: 10rpx 0;
+      border-bottom: 10rpx solid #eeeeee;
+
+      .album_cover {
+        flex: 1;
+
+        image {
+          width: 180rpx;
+          height: 180rpx;
+        }
+      }
+
+      .album_info {
+        flex: 3;
+        padding-left: 20rpx;
+        position: relative;
+
+        .album_info_text {
+          width: 100rpx;
+          height: 50rpx;
+          background-color: $color;
+          color: #ffffff;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .album_info_name {
+          padding: 10rpx 0;
+          color: #888888;
+        }
+        .iconfont{
+          font-size: 40rpx;
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          right: 10%;
+          color: #000000;
+        }
+      }
     }
   }
 }
