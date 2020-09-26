86353600983c868ad6a42431c86cc8e24388f7d7
2218675712 <2218675712@qq.com>
2020-09-26 20:05:27 +0800

    2020年09月26日 20:05:15 专辑详情完成

--- src/pages/imgDetail/index.vue
+++ src/pages/imgDetail/index.vue
@@ -12,7 +12,7 @@
     </view>
     <!--    高清大图-->
     <view class="high_img">
-      <image mode="widthFix" :src='imgDetail.newThumb'></image>
+      <image mode="widthFix" :src='imgDetail.thumb'></image>
     </view>
     <!--    点赞-->
     <view class="user_rank">
@@ -24,7 +24,7 @@
       </view>
     </view>
     <!--    专辑-->
-    <view class="album_wrap">
+    <view class="album_wrap" v-if="album.length">
       <view class="album_title"></view>
       <view class="album_list">
         <view class="album_item" v-for="item in album" :key="item.id">
@@ -35,6 +35,80 @@
             <view class="album_info_text">专辑</view>
             <view class="album_info_name">{{item.name}}</view>
             <text class="iconfont iconiconfontjiantou4"></text>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!--    最热评论-->
+    <view class="comment hot" v-if="hot.length">
+      <view class="comment_title">
+        <text class="iconfont iconhot1"></text>
+        <text class="comment_text">最热评论</text>
+      </view>
+      <view class="comment_list">
+        <view class="comment_item" v-for="item in hot" :key="item.id">
+          <view class="comment_user">
+            <view class="user_icon">
+              <image :src="item.user.avatar" mode="widthFix"></image>
+            </view>
+            <view class="user_name">
+              <view class="user_nickname">
+                {{item.user.name}}
+              </view>
+              <view class="user_time">
+                {{item.cnTime }}
+              </view>
+            </view>
+            <view class="user_badge">
+              <image
+                  v-for="item2 in item.user.title"
+                  :key="item2.icon"
+                  :src="item2.icon"></image>
+            </view>
+
+          </view>
+          <view class="comment_desc">
+            <view class="comment_content">{{item.content}}</view>
+            <view class="comment_like">
+              <text class="iconfont icondianzan">{{item.size}}</text>
+            </view>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!--    最新评论-->
+    <view class="comment new" v-if="comment.length">
+      <view class="comment_title">
+        <text class="iconfont iconpinglun"></text>
+        <text class="comment_text">最新评论</text>
+      </view>
+      <view class="comment_list">
+        <view class="comment_item" v-for="item in comment" :key="item.id">
+          <view class="comment_user">
+            <view class="user_icon">
+              <image :src="item.user.avatar" mode="widthFix"></image>
+            </view>
+            <view class="user_name">
+              <view class="user_nickname">
+                {{item.user.name}}
+              </view>
+              <view class="user_time">
+                {{item.cnTime}}
+              </view>
+            </view>
+            <view class="user_badge">
+              <image
+                  v-for="item2 in item.user.title"
+                  :key="item2.icon"
+                  :src="item2.icon"></image>
+            </view>
+
+          </view>
+          <view class="comment_desc">
+            <view class="comment_content">{{item.content}}</view>
+            <view class="comment_like">
+              <text class="iconfont icondianzan">{{item.size}}</text>
+            </view>
           </view>
         </view>
       </view>
@@ -63,11 +137,11 @@ export default {
   onLoad: function () {
     const {imgList, imgIndex} = getApp().globalData
     this.imgDetail = imgList[imgIndex]
-    this.imgDetail.newThumb = this.imgDetail.thumb + this.imgDetail.rule.replace('$<Height>', 375)
     // XX年前的数据
     this.imgDetail.cnTime = moment(this.imgDetail.atime * 1000).fromNow()
     // 获取图片评论
     this.getComments(this.imgDetail.id)
+    console.log(this.imgDetail.id)
   },
   methods: {
     /**
@@ -76,8 +150,13 @@ export default {
      */
     getComments(id) {
       this.request({
-        url: `http://157.122.54.189:9088/image/v2/wallpaper/wallpaper/${id}/comment`,
+        url: `https://service.picasso.adesk.com/v2/wallpaper/wallpaper/5a13d879e7bce743777223e4/comment`
       }).then(result => {
+
+        // 转换时间
+        result.res.hot.forEach(v => v.cnTime = moment(v.atime * 1000).fromNow())
+        result.res.comment.forEach(v => v.cnTime = moment(v.atime * 1000).fromNow())
+
         this.album = result.res.album
         this.hot = result.res.hot
         this.comment = result.res.comment
@@ -185,7 +264,8 @@ export default {
           padding: 10rpx 0;
           color: #888888;
         }
-        .iconfont{
+
+        .iconfont {
           font-size: 40rpx;
           position: absolute;
           top: 50%;
@@ -195,6 +275,99 @@ export default {
         }
       }
     }
+  }
+}
+
+.comment {
+  .comment_title {
+    padding: 15rpx;
+
+    .iconhot1 {
+      color: red;
+      font-size: 40rpx;
+    }
+
+    .iconpinglun {
+      color: aqua;
+      font-size: 40rpx;
+    }
+
+    .comment_text {
+      font-weight: 600;
+      font-size: 28rpx;
+      color: #666666;
+      margin-left: 10rpx;
+    }
+  }
+
+  .comment_list {
+    .comment_item {
+      border-bottom: 15rpx solid #eeeeee;
+
+      .comment_user {
+        display: flex;
+        padding: 20rpx 0;
+
+        .user_icon {
+          width: 15%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          image {
+            width: 90%;
+          }
+        }
+
+        .user_name {
+          flex: 1;
+
+          .user_nickname {
+            color: #777777;
+          }
+
+          .user_time {
+            color: #cccccc;
+            font-size: 24rpx;
+            padding: 5rpx 0;
+          }
+        }
+
+        .user_badge {
+          image {
+            width: 40rpx;
+            height: 40rpx;
+          }
+        }
+
+      }
+
+      .comment_desc {
+        display: flex;
+        padding: 30rpx 0;
+
+        .comment_content {
+          flex: 1;
+          padding-left: 15%;
+          color: #000000;
+        }
+
+        .comment_like {
+          text-align: right;
+
+          .iconfont {
+
+
+          }
+        }
+      }
+    }
+  }
+}
+
+.high_img {
+  image {
+    border-bottom: 1px solid #eeeeee;
   }
 }
 </style>
